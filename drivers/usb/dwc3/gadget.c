d62a2286cf18d5ffddb051d169cedbd6a9391d1e
Ethan Chen <intervigil@gmail.com>
2018-05-20 19:02:19 -0700

    Merge tag 'v3.10.85' into nubia_msm8976_p-tag
    This is the 3.10.85 stable release
    
    Change-Id: I33304a61558b4b06fed244558d54b2df019ca112

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -382,6 +382,9 @@ int dwc3_send_gadget_generic_command(struct dwc3 *dwc, int cmd, u32 param)
 		if (!(reg & DWC3_DGCMD_CMDACT)) {
 			dev_vdbg(dwc->dev, "Command Complete --> %d\n",
 					DWC3_DGCMD_STATUS(reg));
+			if (DWC3_DGCMD_STATUS(reg))
+				ret = -EINVAL;
+				break;
 			ret = 0;
 			break;
 		}
@@ -432,6 +435,8 @@ int dwc3_send_gadget_ep_cmd(struct dwc3 *dwc, unsigned ep,
 			 */
 			if (reg & 0x2000)
 				ret = -EAGAIN;
+			else if (DWC3_DEPCMD_STATUS(reg))
+				ret = -EINVAL;
 			else
 				ret = 0;
 			break;
